5532551b0480975898e9969d4e9e1c94f966804c
rrathbone <rachellerathbone@gmail.com>
2019-05-25 11:08:39 -0700

    initial commit section three

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 // Components
 import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 class BlogIndex extends React.Component {
   render() {
+    const { data } = this.props
+    const blogTitle = data.site.siteMetadata.title
+    const authorName = data.site.siteMetadata.author
+
     return (
       <Layout
-        title="Starter Blog"
+        title={blogTitle}
         subtitle="Built with React and Gatsby"
       >
+        <SEO title="All posts"/>
         <div className="blog-container">
           <section>
             <div className="post-summary">
@@ -36,7 +43,7 @@ class BlogIndex extends React.Component {
           <aside>
             <p>We'll put a profile pic here later</p>
             <h3>
-              Rachelle Rathbone
+              {authorName}
             </h3>
             <p>
               Goat gouda who moved my cheese. Red leicester edam port-salut
@@ -52,3 +59,14 @@ class BlogIndex extends React.Component {
 }
 
 export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+  }
+`
