0fc01b5c81b00fb6519df37fa2f8b87057867721
rrathbone <rachellerathbone@gmail.com>
2019-08-03 15:38:36 -0700

    wip: contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,74 +1,68 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+	const { createPage } = actions;
 
-  const blogPost = path.resolve(`./src/templates/post.js`)
-    return graphql(
-      `
-        {
-          allContentfulPost {
-            edges {
-              node {
-                title
-                subtitle
-                description
-                date
-                 image {
-                  fluid {
-                    src
-                  }
-                }
-                content {
-                  childContentfulRichText {
-                    html
-                  }
-                }
-                slug
-              }
-            }
-          }
-        }
-      `
-    ).then(result => {
-      if (result.errors) {
-        throw result.errors
-      }
+	const blogPost = path.resolve(`./src/templates/post.js`);
+  	// return graphql(
+  	// 	`
+  	//       {
+  	//         allContentfulPosts {
+    //           edges {
+    //             node {
+    //               title
+    //               subtitle
+    //               description
+    //               slug
+    //               date
+    //               content {
+    //                 childContentfulRichText {
+    //                   html
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         }
+  	//       }
+  	//     `
+  	// ).then((result) => {
+  	// 	if (result.errors) {
+  	// 		throw result.errors;
+  	// 	}
 
-      const posts = result.data.allContentfulPost.edges
+  	// 	const posts = result.data.allContentfulPosts.edges;
 
-      posts.forEach((post, index) => {
-        const previous = index === posts.length - 1 ? null : posts[index + 1].node
-        const next = index === 0 ? null : posts[index - 1].node
+  	// 	posts.forEach((post, index) => {
+  	// 		const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+  	// 		const next = index === 0 ? null : posts[index - 1].node;
 
-        createPage({
-          path: post.node.slug,
-          component: blogPost,
+  	// 		createPage({
+  	// 			path: post.node.slug,
+  	// 			component: blogPost,
 
-          context: {
-            slug: post.node.slug,
-            previous,
-            next,
-          },
-        })
-      })
+  	// 			context: {
+  	// 				slug: post.node.slug,
+  	// 				previous,
+  	// 				next
+  	// 			}
+  	// 		});
+  	// 	});
 
-    return null
-  })
-
-}
+  	// 	return null;
+  	// });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+	// const { createNodeField } = actions;
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+	// if (node.internal.type === `MarkdownRemark`) {
+	// 	const value = createFilePath({ node, getNode });
 
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+	// 	createNodeField({
+	// 		name: `slug`,
+	// 		node,
+	// 		value
+	// 	});
+	// }
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,137 @@
 // @ts-nocheck
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 // Components
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 // Utils
-import { shorten } from '../utils/truncateStr'
+import { shorten } from '../utils/truncateStr';
 
 // Assets
 // import profilePic from '../../content/assets/profile-pic.png'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
+const document = {
+	nodeType: 'document',
+	data: {},
+	content: [
+		{
+			nodeType: 'paragraph',
+			data: {},
+			content: [
+				{
+					nodeType: 'text',
+					value: 'Hello world!',
+					marks: [],
+					data: {}
+				}
+			]
+		}
+	]
+};
+
+documentToReactComponents(document); // -> <p>Hello world!</p>
 class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
+	render() {
+		const { data } = this.props;
 
-    const { title, author } = data.site.siteMetadata
-    const posts = data.allContentfulPost.edges
-    const profilePic = data.profilePic.childImageSharp.fluid
+		const { title, author } = data.site.siteMetadata;
+		// const posts = data.allContentfulPosts.edges;
+		const profilePic = data.profilePic.childImageSharp.fluid;
 
-    return (
-      <Layout
-        title={title}
-        subtitle="Built with React and Gatsby"
-      >
-        <SEO title="All posts"/>
-        <div className="blog-container">
-          <section>
-            {posts.map((post, index) => {
-              return (
-                <div className="post-summary" key={index}>
-                  <p>{post.node.date}</p>
-                  <h2>{post.node.title}</h2>
-                  <div className="content" dangerouslySetInnerHTML={{ __html: shorten(post.node.content.childContentfulRichText.html, 200) }} />
-                  <Link to={post.node.slug}>
-                    <button data-gtm="read-more" id={`data::${post.node.slug}`}>Read more</button>
-                  </Link>
-                </div>
-              )
-            })}
-          </section>
-          <aside>
-            <Img fluid={profilePic} alt={`Author ${author}`} />
-            <h3>
-              {author}
-            </h3>
-            <p>
-              Goat gouda who moved my cheese. Red leicester edam port-salut
-              cream cheese pepper jack halloumi jarlsberg mozzarella. Boursin
-              cheese strings manchego bocconcini croque monsieur cheese slices
-              the big cheese fromage.
-            </p>
-          </aside>
-        </div>
-      </Layout>
-    )
-  }
+		return (
+			// <Layout title={title} subtitle="Built with React and Gatsby">
+			<Layout title={title} subtitle="Built with React and Gatsby">
+				<SEO title="All posts" />
+				<div className="blog-container">
+					<section>
+						{/* {posts.map((post, index) => {
+							return (
+								<div className="post-summary" key={index}>
+									<p>{post.node.date}</p>
+									<h2>{post.node.title}</h2>
+									<div
+										className="content"
+										dangerouslySetInnerHTML={{
+											__html: shorten(post.node.content.childContentfulRichText.html, 200)
+										}}
+									/>
+									<Link to={post.node.slug}>
+										<button data-gtm="read-more" id={`data::${post.node.slug}`}>
+											Read more
+										</button>
+									</Link>
+								</div>
+							);
+						})} */}
+					</section>
+					<aside>
+						<Img fluid={profilePic} alt={`Author ${author}`} />
+						<h3>{author}</h3>
+						<p>
+							Goat gouda who moved my cheese. Red leicester edam port-salut cream cheese pepper jack
+							halloumi jarlsberg mozzarella. Boursin cheese strings manchego bocconcini croque monsieur
+							cheese slices the big cheese fromage.
+						</p>
+					</aside>
+				</div>
+			</Layout>
+		);
+	}
 }
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        author
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          html
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
-    profilePic: file(absolutePath: { regex: "/profile-pic.png/" }) {
-      childImageSharp {
-        fluid(maxWidth: 400, maxHeight: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    allContentfulPost {
-      edges {
-        node {
-          title
-          subtitle
-          description
-          date
-           image {
-            fluid {
-              src
-            }
-          }
-          content {
-            childContentfulRichText {
-              html
-            }
-          }
-          slug
-        }
-      }
-    }
-  }
-`
+	query {
+		site {
+			siteMetadata {
+				title
+				author
+			}
+		}
+		profilePic: file(absolutePath: { regex: "/profile-pic.png/" }) {
+			childImageSharp {
+				fluid(maxWidth: 400, maxHeight: 300) {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+	}
+`;
+
+// allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+// 	edges {
+// 		node {
+// 			excerpt
+// 			fields {
+// 				slug
+// 			}
+// 			html
+// 			frontmatter {
+// 				date(formatString: "MMMM DD, YYYY")
+// 				title
+// 				description
+// 			}
+// 		}
+// 	}
+// }
+// allContentfulPosts {
+// 	edges {
+// 		node {
+// 			title
+// 			subtitle
+// 			description
+// 			date
+// 			content {
+// 				childContentfulRichText {
+// 					html
+// 				}
+// 			}
+// 			slug
+// 		}
+// 	}
+// }
