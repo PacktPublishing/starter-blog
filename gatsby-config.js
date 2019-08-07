5c269dd7ed67da55abfd1685a3c3daff7242587d
rrathbone <rachellerathbone@gmail.com>
2019-08-07 08:21:34 -0700

    wip - section-three

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,66 @@
 module.exports = {
-  siteMetadata: {
-    title: `Starter Blog`,
-    author: `Rachelle Rathbone`,
-    description: `A blog that shows you the awesome power of gatsby.`,
-    social: {
-      twitter: `coding_love`,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/posts`,
-        name: `blogPosts`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Starter Blog`,
-        short_name: `Blog`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `content/assets/cheese-icon.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {},
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-offline`
-  ],
-}
+	siteMetadata: {
+		title: `Starter Blog`,
+		author: `Rachelle Rathbone`,
+		description: `A blog that shows you the awesome power of gatsby.`,
+		social: {
+			twitter: `coding_love`
+		},
+		siteUrl: `http://localhost:9000`
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/assets`,
+				name: `assets`
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/posts`,
+				name: `blogPosts`
+			}
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Starter Blog`,
+				short_name: `Blog`,
+				start_url: `/`,
+				background_color: `#f7f0eb`,
+				theme_color: `#a2466c`,
+				display: `standalone`,
+				icon: `content/assets/cheese-icon.png`
+			}
+		},
+		{
+			resolve: `gatsby-plugin-typography`,
+			options: {
+				pathToConfigModule: `src/utils/typography`
+			}
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {}
+		},
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-offline`,
+		`gatsby-plugin-sitemap`,
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: ``,
+				head: true
+			}
+		},
+		{
+			resolve: `gatsby-plugin-google-tagmanager`,
+			options: {
+				id: ``,
+				includeInDevelopment: true
+			}
+		}
+	]
+};
