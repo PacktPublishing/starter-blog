92b74ca062c099530586dce2a0bd45ed223c8426
rrathbone <rachellerathbone@gmail.com>
2019-08-17 13:43:15 -0700

    complete section-four

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 // Components
 import Layout from '../components/layout';
@@ -7,10 +7,34 @@ import SEO from '../components/seo';
 
 class PostTemplate extends React.Component {
 	render() {
+		const frontmatter = this.props.data.markdownRemark.frontmatter;
+		const { title, subtitle, description, date } = frontmatter;
+		const post = this.props.data.markdownRemark;
+		const { previous, next, slug } = this.props.pageContext;
+
 		return (
-			<Layout title="A title" subtitle="A subtitle">
-				<SEO title="Another title" description="A description" />
-				<section className="posts">Things will happen here soon.</section>
+			<Layout title={title} subtitle={subtitle}>
+				<SEO title={title} description={description || post.excerpt} slug={slug}/>
+				<section className="posts">
+					<p className="date">{date}</p>
+					<div dangerouslySetInnerHTML={{ __html: post.html }} />
+					<ul>
+						<li className="post-navigation">
+							{previous && (
+								<Link to={previous.fields.slug} rel="prev">
+									← {previous.frontmatter.title}
+								</Link>
+							)}
+						</li>
+						<li className="post-navigation">
+							{next && (
+								<Link to={next.fields.slug} rel="next">
+									{next.frontmatter.title} →
+								</Link>
+							)}
+						</li>
+					</ul>
+				</section>
 			</Layout>
 		);
 	}
@@ -19,16 +43,17 @@ class PostTemplate extends React.Component {
 export default PostTemplate;
 
 export const pageQuery = graphql`
- query Posts($slug: String!) {
-   markdownRemark(fields: { slug: { eq: $slug } }) {
-     id
-     excerpt(pruneLength: 160)
-     html
-     frontmatter {
-       title
-       date(formatString: "MMMM DD, YYYY")
-       description
-     }
-   }
- }
-`
+	query Posts($slug: String!) {
+		markdownRemark(fields: { slug: { eq: $slug } }) {
+			id
+			excerpt(pruneLength: 160)
+			html
+			frontmatter {
+				title
+				date(formatString: "MMMM DD, YYYY")
+				subtitle
+				description
+			}
+		}
+	}
+`;
