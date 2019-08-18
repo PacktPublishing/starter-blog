7b27bd65619593e0502fcf9752966f0f5f2de6b1
rrathbone <rachellerathbone@gmail.com>
2019-08-17 20:07:19 -0700

    section four

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 
 // Components
 import Layout from '../components/layout';
@@ -7,34 +7,10 @@ import SEO from '../components/seo';
 
 class PostTemplate extends React.Component {
 	render() {
-		const frontmatter = this.props.data.markdownRemark.frontmatter;
-		const { title, subtitle, description, date } = frontmatter;
-		const post = this.props.data.markdownRemark;
-		const { previous, next, slug } = this.props.pageContext;
-
 		return (
-			<Layout title={title} subtitle={subtitle}>
-				<SEO title={title} description={description || post.excerpt} slug={slug}/>
-				<section className="posts">
-					<p className="date">{date}</p>
-					<div dangerouslySetInnerHTML={{ __html: post.html }} />
-					<ul>
-						<li className="post-navigation">
-							{previous && (
-								<Link to={previous.fields.slug} rel="prev">
-									← {previous.frontmatter.title}
-								</Link>
-							)}
-						</li>
-						<li className="post-navigation">
-							{next && (
-								<Link to={next.fields.slug} rel="next">
-									{next.frontmatter.title} →
-								</Link>
-							)}
-						</li>
-					</ul>
-				</section>
+			<Layout title="A title" subtitle="A subtitle">
+				<section className="posts">Things will happen here soon.</section>
+				<SEO title="Another title" description="A description" />
 			</Layout>
 		);
 	}
