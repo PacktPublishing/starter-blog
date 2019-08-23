48427844af77faff06447c704bfd01bde7dd4bf1
rrathbone <rachellerathbone@gmail.com>
2019-08-22 23:09:12 -0700

    update app with home icon link

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,48 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-class Layout extends React.Component {
-  render() {
-    const { title, subtitle, children } = this.props
+function Layout({ title, subtitle, children }) {
+	const { home } = useStaticQuery(
+		graphql`
+			query {
+				home: file(absolutePath: { regex: "/home.png/" }) {
+					childImageSharp {
+						fluid(maxWidth: 10, maxHeight: 10) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+			}
+		`
+	);
 
-    return (
-      <div className="app-container">
-        <header>
-          <h1>
-            {title}
-          </h1>
-          <p>{subtitle}</p>
-        </header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Rachelle Rathbone
-        </footer>
-      </div>
-    )
-  }
+	const homeIcon = home.childImageSharp.fluid;
+
+	return (
+		<div className="app-container">
+			<div className="homeLinkContainer">
+				{title !== 'Starter Blog' && (
+					<Link to="/">
+						<Img fluid={homeIcon} alt="Link to go to home page" />
+					</Link>
+				)}
+			</div>
+			<header>
+				<h1>{title}</h1>
+				<p>{subtitle}</p>
+			</header>
+			<main>{children}</main>
+			<footer>© {new Date().getFullYear()}, Rachelle Rathbone</footer>
+		</div>
+	);
 }
 
-export default Layout
+Layout.propTypes = {
+	title: PropTypes.string.isRequired,
+	subtitle: PropTypes.string.isRequired,
+	children: PropTypes.node.isRequired
+};
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -58,6 +58,7 @@ export const pageQuery = graphql`
 		site {
 			siteMetadata {
 				title
+				subtitle
 				author
 			}
 		}
